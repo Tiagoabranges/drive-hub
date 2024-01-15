bde486db9b386d589f7ba67e45f423491beb9014
tiagoabranges <tiagoabranges@gmail.com>
2024-01-15 17:06:53 -0300

    feat: import prisma-service in the module

--- src/automobile-usage/automobile-usage.module.ts
+++ src/automobile-usage/automobile-usage.module.ts
@@ -1,9 +1,12 @@
+// src/automobile-usage/automobile-usage.module.ts
+
 import { Module } from '@nestjs/common';
 import { AutomobileUsageService } from './automobile-usage.service';
 import { AutomobileUsageController } from './automobile-usage.controller';
+import { PrismaService } from 'src/prisma.service';
 
 @Module({
   controllers: [AutomobileUsageController],
-  providers: [AutomobileUsageService]
+  providers: [AutomobileUsageService, PrismaService],
 })
 export class AutomobileUsageModule {}
